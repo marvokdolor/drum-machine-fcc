727e19bd9421b8450069f603e50483bc2ca4adc9
marvokdolor <marvourneen@mkdolor.com>
2019-01-15 22:00:20 -0500

    Updates

---
+++ precache-manifest.03169e8ab5f6474062fda14fa28ca555.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ef9e57f94ac8ad00057",
+    "url": "/drum-machine-fcc/static/js/runtime~main.1ef9e57f.js"
+  },
+  {
+    "revision": "820e00b85b600afb1b6d",
+    "url": "/drum-machine-fcc/static/js/main.820e00b8.chunk.js"
+  },
+  {
+    "revision": "b37b0e24dc7a40cb64e3",
+    "url": "/drum-machine-fcc/static/js/1.b37b0e24.chunk.js"
+  },
+  {
+    "revision": "820e00b85b600afb1b6d",
+    "url": "/drum-machine-fcc/static/css/main.d8f92202.chunk.css"
+  },
+  {
+    "revision": "4aebcb1b5c81b8e3f1284d54df24ea04",
+    "url": "/drum-machine-fcc/index.html"
+  }
+];
